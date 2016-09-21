fa47d7362895b58d41cb8027a46257ebb2e70e19
sjm <sujinming0125@163.com>
2016-09-21 10:23:24 +0800

    add guide

--- app/src/main/java/com/example/sujinming/qumiao/MainActivity.java
+++ app/src/main/java/com/example/sujinming/qumiao/MainActivity.java
@@ -1,13 +1,59 @@
 package com.example.sujinming.qumiao;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity {
+
+    private ViewPager viewPager;
+    private int[] imgIds = new int[]{R.drawable.guide_image1,
+        R.drawable.guide_image2, R.drawable.guide_image3};
+
+    private List<ImageView> images = new ArrayList<ImageView>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager)findViewById(R.id.viewPager);
+        viewPager.setPageTransformer(true, new DepthPageTransformer());
+        viewPager.setAdapter(new PagerAdapter() {
+
+            @Override
+            public Object instantiateItem(ViewGroup container, int position) {
+                ImageView imageView = new ImageView(MainActivity.this);
+                imageView.setImageResource(imgIds[position]);
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                container.addView(imageView);
+                images.add(imageView);
+                return imageView;
+            }
+
+            @Override
+            public void destroyItem(ViewGroup container, int position, Object object) {
+                container.removeView(images.get(position));
+            }
+
+            @Override
+            public int getCount() {
+                return imgIds.length;
+            }
+
+            @Override
+            public boolean isViewFromObject(View view, Object object) {
+                return view == object;
+            }
+        });
     }
 }
