e8e4abd69389ed0156ac0dd48bb9dd94316cf7ff
sjm <sujinming0125@163.com>
2016-09-21 10:43:25 +0800

    change guide as fullscreen

--- app/src/main/java/com/example/sujinming/qumiao/MainActivity.java
+++ app/src/main/java/com/example/sujinming/qumiao/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
@@ -23,7 +24,8 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         viewPager = (ViewPager)findViewById(R.id.viewPager);
