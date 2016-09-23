29227ff9c6fb3f54cfd53f782a06c987a58741fa
sjm <sujinming0125@163.com>
2016-09-23 22:14:05 +0800

    update the way of guide

--- app/src/main/java/com/example/sujinming/qumiao/MainActivity.java
+++ app/src/main/java/com/example/sujinming/qumiao/MainActivity.java
@@ -4,11 +4,12 @@ import android.app.Activity;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
 import android.view.WindowManager;
-import android.widget.ImageView;
+
+import com.example.sujinming.qumiao.myDefine.DepthPageTransformer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,10 +17,7 @@ import java.util.List;
 public class MainActivity extends Activity {
 
     private ViewPager viewPager;
-    private int[] imgIds = new int[]{R.drawable.guide_image1,
-        R.drawable.guide_image2, R.drawable.guide_image3};
-
-    private List<ImageView> images = new ArrayList<ImageView>();
+    private List<View> views = new ArrayList<View>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,26 +28,26 @@ public class MainActivity extends Activity {
 
         viewPager = (ViewPager)findViewById(R.id.viewPager);
         viewPager.setPageTransformer(true, new DepthPageTransformer());
+        LayoutInflater li = getLayoutInflater();
+        views.add(li.inflate(R.layout.view_page_1, null, false));
+        views.add(li.inflate(R.layout.view_page_2, null, false));
+        views.add(li.inflate(R.layout.view_page_3, null, false));
         viewPager.setAdapter(new PagerAdapter() {
 
             @Override
             public Object instantiateItem(ViewGroup container, int position) {
-                ImageView imageView = new ImageView(MainActivity.this);
-                imageView.setImageResource(imgIds[position]);
-                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-                container.addView(imageView);
-                images.add(imageView);
-                return imageView;
+                container.addView(views.get(position));
+                return views.get(position);
             }
 
             @Override
             public void destroyItem(ViewGroup container, int position, Object object) {
-                container.removeView(images.get(position));
+                container.removeView(views.get(position));
             }
 
             @Override
             public int getCount() {
-                return imgIds.length;
+                return views.size();
             }
 
             @Override
@@ -57,5 +55,7 @@ public class MainActivity extends Activity {
                 return view == object;
             }
         });
+
+
     }
 }
--- app/src/main/java/com/example/sujinming/qumiao/myDefine/DepthPageTransformer.java
+++ app/src/main/java/com/example/sujinming/qumiao/myDefine/DepthPageTransformer.java
@@ -1,4 +1,4 @@
-package com.example.sujinming.qumiao;
+package com.example.sujinming.qumiao.myDefine;
 
 import android.support.v4.view.ViewPager;
 import android.view.View;
